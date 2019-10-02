c3ca1e121c3bb97b05eed73a8a7cf2b71d43c2da
EC2 Default User <ec2-user@ip-172-31-8-240.us-east-2.compute.internal>
2019-10-02 05:42:00 +0000

    finish 2-3

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -58,4 +58,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  
+  # devise の設定
+  config.action_mailer.default_url_option = { host: "localhost", post: ENV.fetch('PORT', 3000) }
 end
